3bc07f56d02c8ca130aa30211597faf48a054cec
luccccccyyyyyy <luccccccyyyyyy@gmail.com>
2025-07-03 22:36:36 -0400

    init functions

--- fileconversion.py
+++ fileconversion.py
@@ -7,6 +7,13 @@ import glob
 # move anything that contains _0000 into imagesTr
 # move rest into labelsTr
 
+os.makedirs("nnUNet_results")
+os.makedirs("nnUNet_raw")
+os.makedirs("nnUNet_proprocessed")
+os.makedirs("nnUNet_raw\\Dataset001_Pan\\imagesTr", parents=True, exist_ok=True)
+os.makedirs("nnUNet_raw\\Dataset001_Pan\\imagesTs")
+os.makedirs("nnUNet_raw\\Dataset001_Pan\\labelsTr")
+shutil.copy("dataset.json", "nnUNet_raw\\Dataset001_Pan\\dataset.json")
 
 for subtype in ['subtype0', 'subtype1', 'subtype2']:
     for f in glob.glob(f"data\\train\\{subtype}\\*"):
@@ -16,17 +23,13 @@ for subtype in ['subtype0', 'subtype1', 'subtype2']:
             dest = "imagesTr"
         else:
             dest = "labelsTr"
-        
-
         source = f
         dest = os.path.join(f"nnUNet_raw\\{dest}",filename)
-        
         #print(f"Moving {source} to {dest}")
         #shutil.copy(source, dest)
-
 
 for f in glob.glob(f"data\\test\\*"):
     filename = f.split("\\")[-1]
     dest = os.path.join("nnUNet_raw\\imagesTs",filename)
     #print(f"moving {f} to {dest}")
-    shutil.copy(f,dest)
+    #shutil.copy(f,dest)

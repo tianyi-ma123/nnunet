4478ff39134bb8c61e2a5b0e0f3d8bf51759b48f
tianyi-ma123 <ty.matianyi@gmail.com>
2025-07-05 01:10:13 -0400

    delete dataset.json and update fileconversion.py

--- fileconversion.py
+++ fileconversion.py
@@ -13,7 +13,7 @@ for folder in [
     f"{DATASET}\\labelsTr",]:
     os.makedirs(folder, exist_ok=True)
 
-shutil.copy("dataset.json", f"{DATASET}\\dataset.json")
+#shutil.copy("dataset.json", f"{DATASET}\\dataset.json")
 
 for subtype in ['subtype0', 'subtype1', 'subtype2']:
     for f in glob.glob(f"data\\train\\{subtype}\\*"):
@@ -28,8 +28,49 @@ for subtype in ['subtype0', 'subtype1', 'subtype2']:
         #print(f"Moving {source} to {dest}")
         shutil.copy(source, dest)
 
+for subtype in ['subtype0', 'subtype1', 'subtype2']:
+    for f in glob.glob(f"data\\validation\\{subtype}\\*"):
+        filename = f.split("\\")[-1]
+
+        if "_0000" in f:
+            dest = "imagesTr"
+        else:
+            dest = "labelsTr"
+        source = f
+        dest = os.path.join(f"{DATASET}\\{dest}",filename)
+        #print(f"Moving {source} to {dest}")
+        shutil.copy(source, dest)       
+
 for f in glob.glob(f"data\\test\\*"):
     filename = f.split("\\")[-1]
     dest = os.path.join(f"{DATASET}\\imagesTs",filename)
     #print(f"moving {f} to {dest}")
     shutil.copy(f,dest)
+
+count = 0
+for f in glob.glob(f"{DATASET}\\imagesTr\\*"):
+    count = count + 1
+
+
+print(count)  #check for number of training files
+
+
+dataset_dic = { 
+ "channel_names": { 
+   "0": "CT" 
+   
+ }, 
+ "labels": {
+   "background": 0.0,
+   "Pan": 1.0,
+   "Lesion": 2.0
+ }, 
+ "numTraining": count, 
+ "file_ending": ".nii.gz",
+ "overwrite_image_reader_writer": "SimpleITKIO"  
+ }
+
+with open(f"{DATASET}\\dataset.json", 'w') as f:
+    json.dump(dataset_dic,f, indent=2, sort_keys=False)
+
+
